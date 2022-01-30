4a8a329caf616a266dd2e4d05acab2bc338d29ac
Reichan Muhammad <67642923+saikocode@users.noreply.github.com>
2022-01-30 18:05:33 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->smallInteger('type')->default('1');
             $table->string('password');
+            $table->date('birth_date');
             $table->text('address')->nullable();
             $table->string('phone_number')->nullable();
             $table->rememberToken();
