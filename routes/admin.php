c705c69c8504a9dea69879ed6cf580688811badb
Reichan Muhammad <reichan.muhammad@gmail.com>
2022-01-31 08:29:02 +0700

    issue#7-creating-route-for-admin-dashboard

---
+++ routes/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+});
